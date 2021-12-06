1489b0cf22d7d8c0e6601f4b871eb7d54f3725fc
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2021-12-06 10:22:13 +0100

    Add server docker config fix (#3)

--- server/start.sh
+++ server/start.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# path = "SolidTrade-Dev";
 dockername="solidtrade-dev-server";
 port=8007;
 
 if [ "$1" = "production" ]; then
-#   path = "SolidTrade";
   dockername="solidtrade-server";
   port=8008;
 fi
 
-# cp /root/projects/Rose-Linode/env/$path/server/.env .
-
 # Stop current docker image
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=$dockername --format="{{.ID}}"))
+docker stop $(docker ps | awk '{split($2,image,":"); print $1, image[1]}' | awk -v image=$dockername '$2 == image {print $1}')
 
 # Build & Run Image
 docker build -t $dockername . && docker run -it -d -p $port:80 $dockername && docker ps
