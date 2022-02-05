daead0a8b7b7a02e0086a6fa265e39bf78b6463d
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-02-05 19:10:45 +0100

    Fix bug when unsubscribing would not work

--- client/lib/services/stream/tr_product_price_service.dart
+++ client/lib/services/stream/tr_product_price_service.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:get_it/get_it.dart';
 import 'package:rxdart/subjects.dart';
 import 'package:solidtrade/data/common/error/request_response.dart';
 import 'package:solidtrade/data/common/shared/constants.dart';
 import 'package:solidtrade/data/common/shared/tr/tr_product_info.dart';
 import 'package:solidtrade/data/common/shared/tr/tr_product_price.dart';
+import 'package:solidtrade/data/common/shared/tr/tr_request_response.dart';
 import 'package:solidtrade/services/request/data_request_service.dart';
 import 'package:solidtrade/services/stream/base/base_service.dart';
 import 'package:solidtrade/services/stream/tr_product_info_service.dart';
@@ -12,19 +15,13 @@ class TrProductPriceService extends BaseService<RequestResponse<TrProductPrice>?
   TrProductPriceService() : super(BehaviorSubject.seeded(null));
 
   void requestTrProductPrice(String isinWithExtention) async {
-    DataRequestService.trApiDataRequestService
-        .makeRequestAsync<TrProductPrice>(
-      Constants.getTrProductPriceRequestString(isinWithExtention),
-    )
-        .listen((event) {
-      if (behaviorSubject.hasListener) {
-        behaviorSubject.add(event.requestResponse);
-        return;
-      }
+    StreamSubscription<TrRequestResponse<TrProductPrice>>? subscription;
 
-      DataRequestService.trApiDataRequestService.unsub(event.id);
-      behaviorSubject.close();
-    });
+    subscription = DataRequestService.trApiDataRequestService
+        .makeRequestAsync<TrProductPrice>(
+          Constants.getTrProductPriceRequestString(isinWithExtention),
+        )
+        .listen((event) => onEvent(event, subscription));
   }
 
   Future<RequestResponse<TrProductInfo>> requestTrProductPriceByIsinWithoutExtention(String isinWithoutExtention) async {
@@ -36,20 +33,25 @@ class TrProductPriceService extends BaseService<RequestResponse<TrProductPrice>?
 
     var isinWithExtention = "${info.result!.isin}.${info.result!.exchangeIds.first}";
 
-    DataRequestService.trApiDataRequestService
-        .makeRequestAsync<TrProductPrice>(
-      Constants.getTrProductPriceRequestString(isinWithExtention),
-    )
-        .listen((event) {
-      if (behaviorSubject.hasListener) {
-        behaviorSubject.add(event.requestResponse);
-        return;
-      }
+    StreamSubscription<TrRequestResponse<TrProductPrice>>? subscription;
 
-      DataRequestService.trApiDataRequestService.unsub(event.id);
-      behaviorSubject.close();
-    });
+    subscription = DataRequestService.trApiDataRequestService
+        .makeRequestAsync<TrProductPrice>(
+          Constants.getTrProductPriceRequestString(isinWithExtention),
+        )
+        .listen((event) => onEvent(event, subscription));
 
     return info;
+  }
+
+  void onEvent(TrRequestResponse<TrProductPrice> event, StreamSubscription<TrRequestResponse<TrProductPrice>>? subscription) {
+    if (behaviorSubject.hasListener) {
+      behaviorSubject.add(event.requestResponse);
+      return;
+    }
+
+    subscription!.cancel();
+    DataRequestService.trApiDataRequestService.unsub(event.id);
+    behaviorSubject.close();
   }
 }
