ce521d2cf0d517ea32fd96837ca71a2ba88eb1b8
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-04-02 12:46:33 +0200

    refactor(database): :card_file_box: add migration for fractional shares support

--- server/Migrations/20220402103904_AllowFractionalShares.Designer.cs
+++ server/Migrations/20220402103904_AllowFractionalShares.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SolidTradeServer.Data.Common;
 
 namespace SolidTradeServer.Migrations
 {
     [DbContext(typeof(DbSolidTrade))]
-    partial class DbSolidTradeModelSnapshot : ModelSnapshot
+    [Migration("20220402103904_AllowFractionalShares")]
+    partial class AllowFractionalShares
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
