42344f4925533867c3524650f47b3462a9bf8d87
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-04-02 17:56:29 +0200

    refactor: :fire: remove unused code

--- client/lib/services/util/tr_util.dart
+++ client/lib/services/util/tr_util.dart
@@ -86,44 +86,6 @@ class TrUtil {
 
     return false;
   }
-
-  static Map<DateTime, HistoricalPosition> getCurrentNumberOfSharesForPortfolioPosition(List<DateTime> dates, List<HistoricalPosition> pps) {
-    final Map<DateTime, HistoricalPosition> map = {};
-    List<HistoricalPosition> positions = [];
-    List<int> ids = [];
-
-    var d = dates.reduce((a, b) => a.difference(pps.first.createdAt).abs() < b.difference(pps.first.createdAt).abs() ? a : b);
-
-    var index = dates.indexWhere((element) => element == d);
-
-    for (var i = index; i < dates.length; i++) {
-      for (var pp in pps) {
-        if (!pp.createdAt.compareTo(dates[i]).isNegative || ids.any((id) => id == pp.id)) {
-          continue;
-        }
-
-        var index = positions.indexWhere((element) => element.isin == pp.isin);
-
-        if (index == -1) {
-          ids.add(pp.id);
-          positions.add(pp);
-          map[dates[i]] = pp;
-          continue;
-        }
-        var pos = positions[index];
-        if (pp.buyOrSell == BuyOrSell.buy) {
-          positions[index] = HistoricalPosition.copy(pos, pos.numberOfShares + pp.numberOfShares);
-        } else {
-          positions[index] = HistoricalPosition.copy(pos, pos.numberOfShares - pp.numberOfShares);
-        }
-
-        ids.add(pp.id);
-        map[dates[i]] = positions[index];
-      }
-    }
-
-    return map;
-  }
 }
 
 class TrUiProductDetails {
