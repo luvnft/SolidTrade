d05df8fea5ea042e5bebffa8b780947da1e1931e
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-04-04 23:00:53 +0200

    build(web): :green_heart: still trying to fix CI build issues 10

--- client/vercel.sh
+++ client/vercel.sh
@@ -2,7 +2,7 @@
 mkdir ./flutter-sdk && cd ./flutter-sdk
 
 # Add Flutter
-git clone -b master https://github.com/flutter/flutter.git
+git clone -b stable https://github.com/flutter/flutter.git
 
 # Add flutter to path
 export PATH="$PATH:`pwd`/flutter/bin"
