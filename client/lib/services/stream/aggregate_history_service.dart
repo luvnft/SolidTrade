9dbda11a39e1620161f83e56270ee66a13687c7d
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-04-17 11:41:37 +0200

    docs: :bulb: explain missing IService

--- client/lib/services/stream/aggregate_history_service.dart
+++ client/lib/services/stream/aggregate_history_service.dart
@@ -3,7 +3,7 @@ import 'package:solidtrade/data/common/shared/constants.dart';
 import 'package:solidtrade/data/common/shared/tr/tr_aggregate_history.dart';
 import 'package:solidtrade/services/request/data_request_service.dart';
 
-// TODO: Use the IService interface
+// We are not using the IService because we won't be needing the BehaviorSubject nor any streams.
 class AggregateHistoryService {
   final Map<String, TrAggregateHistory> _histories = {};
 
