e58c81b8b633760e3e80e3b8cbae6a227516b185
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-04-28 22:08:28 +0200

    fix: :bug: fixes bug when MultipartRequest would not use the correct method type

--- client/lib/services/request/base/base_request_service.dart
+++ client/lib/services/request/base/base_request_service.dart
@@ -155,7 +155,7 @@ abstract class IBaseRequestService {
       ...headers,
     };
 
-    var request = http.MultipartRequest("POST", uri);
+    var request = http.MultipartRequest(method.name, uri);
     request.fields.addAll(fields);
 
     for (var file in files.entries) {
