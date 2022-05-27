36adee826373dccd0203730656baf79f8a345ad7
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-05-27 21:00:34 +0200

    fix: :sparkles: add local auth for biometric user authentication

--- client/android/app/src/main/kotlin/com/rosemote/solidtrade/MainActivity.kt
+++ client/android/app/src/main/kotlin/com/rosemote/solidtrade/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.rosemite.solidtrade
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
---
+++ client/lib/services/util/local_auth_util.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:solidtrade/services/util/debug/log.dart';
+
+class UtilLocalAuth {
+  static final LocalAuthentication _auth = LocalAuthentication();
+
+  static Future<bool?> authenticate() async {
+    if (!await _auth.isDeviceSupported()) {
+      return null;
+    }
+
+    var authenticated = false;
+    try {
+      authenticated = await _auth.authenticate(
+        localizedReason: 'Scan your fingerprint (or face or whatever) to authenticate',
+        options: const AuthenticationOptions(
+          useErrorDialogs: true,
+          stickyAuth: true,
+        ),
+      );
+    } on PlatformException catch (e) {
+      Log.w("Error occurred when trying to authenticate user. See following Exception for more info");
+      Log.w(e);
+      return false;
+    }
+
+    return authenticated;
+  }
+}
