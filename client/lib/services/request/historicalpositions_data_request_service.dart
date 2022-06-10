31b4958c0cdbab7c7b51b36c6307ba63915a482d
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-06-10 22:51:22 +0200

    fix: :bug: add missing json decode process

--- client/lib/services/request/historicalpositions_data_request_service.dart
+++ client/lib/services/request/historicalpositions_data_request_service.dart
@@ -2,6 +2,7 @@ import 'package:solidtrade/data/entities/historicalposition.dart';
 import 'package:solidtrade/data/models/common/constants.dart';
 import 'package:solidtrade/data/models/request_response/request_response.dart';
 import 'package:solidtrade/services/request/base/base_request_service.dart';
+import 'dart:convert';
 
 class HistoricalPositionsDataRequestService extends IBaseRequestService {
   Future<RequestResponse<List<HistoricalPosition>>> fetchHistoricalPosition(int userId) async {
@@ -9,7 +10,7 @@ class HistoricalPositionsDataRequestService extends IBaseRequestService {
       HttpMethod.get,
       Constants.endpointHistoricalPosition + userId.toString(),
     ).createCustom((input) {
-      var data = input as List<dynamic>;
+      var data = jsonDecode(input.body) as List<dynamic>;
       var result = data.map((e) => HistoricalPosition.fromJson(e));
       return result.toList();
     });
