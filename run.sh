777e997396e28b97d6e814993cd46afcca570b05
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-08-28 19:53:14 +0200

    ci: :rocket: finalize new CI/CD bash script and add new dev pipeline

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
+branch_name=$1
+environment_name=solidtrade-$branch_name
+credentials_folder=$environment_name
+
+# Select general development folder for feature branches
+if [[ "$branch_name" != "stable" && "$branch_name" != "staging" ]]; then
+  credentials_folder="solidtrade-development"
+fi
+
+# Copy configuration and credentials 
+cp "/root/projects/Rose-Linode/env/${credentials_folder}/.env" '.'
+cp "/root/projects/Rose-Linode/env/${credentials_folder}/server/appsettings.credentials.json" './server/Configuration'
+cp "/root/projects/Rose-Linode/env/${credentials_folder}/server/solid-trade-firebase-credentials.json" './server/Configuration'
+
 # We do not require ngrok for production. This is why we scale down to 0.
-docker-compose up --scale ngrok=0
+docker-compose --project-name $environment_name up --scale ngrok=0
