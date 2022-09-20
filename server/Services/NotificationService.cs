f48658d1a858fdc940694a80fe8a282a4b21f5d4
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-09-20 12:56:44 +0200

    refactor: :memo: log error if registration token is empty

--- server/Services/NotificationService.cs
+++ server/Services/NotificationService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using FirebaseAdmin.Messaging;
 using Serilog;
@@ -8,8 +7,8 @@ namespace SolidTradeServer.Services
 {
     public class NotificationService
     {
-        private readonly ILogger _logger = Log.ForContext<NotificationService>();
         private readonly FirebaseMessaging _fireMessaging = FirebaseMessaging.DefaultInstance;
+        private readonly ILogger _logger = Log.ForContext<NotificationService>();
 
         public Task SendNotification(int userId, string registrationToken, string title, string message)
         {
@@ -20,6 +19,12 @@ namespace SolidTradeServer.Services
             };
             
             _logger.Information("Send notification to user with id {@UserId} with message {@Data}", userId, data);
+
+            if (registrationToken == string.Empty)
+            {
+                _logger.Error("The provided user registration token is empty. There for not sending a notification.");
+                return Task.CompletedTask;
+            }
             
             return _fireMessaging.SendMulticastAsync(new MulticastMessage
             {
