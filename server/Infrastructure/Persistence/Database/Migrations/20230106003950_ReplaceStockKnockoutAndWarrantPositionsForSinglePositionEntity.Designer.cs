f27747cd8abb5a9d70b667f4c675ca6402b7e7b6
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-01-06 01:45:01 +0100

    feat: :card_file_box: create migration to replace different kind of position entities to one

--- server/Infrastructure/Persistence/Database/Migrations/20230106003950_ReplaceStockKnockoutAndWarrantPositionsForSinglePositionEntity.Designer.cs
+++ server/Infrastructure/Persistence/Database/Migrations/20230106003950_ReplaceStockKnockoutAndWarrantPositionsForSinglePositionEntity.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Persistence.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Persistence.Database.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class DbSolidTradeModelSnapshot : ModelSnapshot
+    [Migration("20230106003950_ReplaceStockKnockoutAndWarrantPositionsForSinglePositionEntity")]
+    partial class ReplaceStockKnockoutAndWarrantPositionsForSinglePositionEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
