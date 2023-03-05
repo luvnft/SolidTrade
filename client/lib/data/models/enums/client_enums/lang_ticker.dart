c07bd669e16df76cfc180bdd5fa4c21f94f1697a
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-03-05 10:58:14 +0100

    fix: :bug: resolve errors in app.dart

--- client/lib/data/models/enums/client_enums/lang_ticker.dart
+++ client/lib/data/models/enums/client_enums/lang_ticker.dart
@@ -1,4 +1,17 @@
+import 'package:flutter/material.dart';
+
 enum LanguageTicker {
   en,
   de,
+}
+
+extension LanguageTickerExtension on LanguageTicker {
+  Locale get locale {
+    switch (this) {
+      case LanguageTicker.en:
+        return const Locale('en');
+      case LanguageTicker.de:
+        return const Locale('de');
+    }
+  }
 }
