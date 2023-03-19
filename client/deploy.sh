30df97d2073229b692ab325f0b1181433f5a6549
Solomon Rosemite <solomonrosemiteprojects@gmail.com>
2023-03-20 00:35:17 +0100

    ci: :green_heart: fix ci 6

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-# Add Flutter
+cd client
+
+# Install flutter
 git clone -b 3.7.3 https://github.com/flutter/flutter/ flutter-sdk
 export PATH="$PATH:`pwd`/flutter-sdk/bin"
 
 flutter doctor
-
-echo "$VERCEL_GIT_COMMIT_REF"
-echo "$VERCEL_GIT_COMMIT_REF"
-echo "$VERCEL_GIT_COMMIT_REF"
 
 # Create config directory if it doesn't exist
 mkdir -p ./assets/config
@@ -23,8 +21,13 @@ yum install wget
 wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O yq
 chmod +x yq
 
+branch=$(echo $VERCEL_GIT_COMMIT_REF | cut -d'/' -f2)
+apiUrl="solidtrade.$branch.api.rosemite.dev"
+
+echo "Using $apiUrl as base api url"
+
 # Update base api url
-./yq e '.baseUrl = "urlhere"' ./assets/config/config.yml > ./assets/config/config.yml
+./yq e ".baseUrl = \"$apiUrl\"" ./assets/config/config.yml > ./assets/config/config.yml
 
 # Install dependencies
 flutter pub get
