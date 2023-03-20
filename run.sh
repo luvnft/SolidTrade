6dbc39a7f6a25e5e8c0be30ebf11e550d5914ace
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-03-20 21:39:09 +0100

    ci: :construction_worker: remove default development config

--- run.sh
+++ run.sh
@@ -5,11 +5,6 @@ branch_name=${1,,}
 environment_name=solidtrade-$branch_name
 credentials_folder=$environment_name
 
-# Select general development folder for feature branches
-if [[ "$branch_name" != "stable" && "$branch_name" != "staging" ]]; then
-  credentials_folder="solidtrade-development"
-fi
-
 # Copy configuration and credentials 
 cp ~/projects/Rose-Linode/env/${credentials_folder}/.env '.'
 cp ~/projects/Rose-Linode/env/${credentials_folder}/server/appsettings.credentials.json './server/WebAPI/Configuration'
