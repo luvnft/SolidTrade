003b0d66b20ce8656211aa9d70bc112d4dea46b8
SolomonRosemite <solomonrosemiteprojects@gmail.com>
2023-04-11 12:06:33 +0200

    feat: :sparkles: add search support for other product types

--- client/lib/services/stream/tr_product_search_service.dart
+++ client/lib/services/stream/tr_product_search_service.dart
@@ -1,3 +1,69 @@
+// TODO: Remove commented code if everything works fine
+// import 'package:rxdart/subjects.dart';
+// import 'package:solidtrade/data/models/common/constants.dart';
+// import 'package:solidtrade/data/models/request_response/request_response.dart';
+// import 'package:solidtrade/data/models/trade_republic/tr_product_search.dart';
+// import 'package:solidtrade/services/request/data_request_service.dart';
+// import 'package:solidtrade/services/stream/base/base_service.dart';
+
+// class TrProductSearchService extends IService<Map<SearchType, RequestResponse<TrProductSearch>>?> {
+//   TrProductSearchService() : super(BehaviorSubject.seeded(null));
+//   final Map<String, Map<SearchType, RequestResponse<TrProductSearch>>> _cache = {};
+
+//   Future<Map<SearchType, RequestResponse<TrProductSearch>>> requestTrProductSearch(String search) async {
+//     Map<SearchType, RequestResponse<TrProductSearch>> results;
+//     if (_cache.containsKey(search)) {
+//       results = (_cache[search]!);
+//     } else {
+//       var stocksFuture = DataRequestService.trApiDataRequestService.makeRequest<TrProductSearch>(Constants.getTrProductSearchRequestString(
+//         search,
+//         SearchType.stock.name,
+//       ));
+
+//       var cryptosFuture = DataRequestService.trApiDataRequestService.makeRequest<TrProductSearch>(Constants.getTrProductSearchRequestString(
+//         search,
+//         SearchType.crypto.name,
+//       ));
+
+//       var derivativesFuture = DataRequestService.trApiDataRequestService.makeRequest<TrProductSearch>(Constants.getTrProductSearchRequestString(
+//         search,
+//         SearchType.derivatives.name,
+//       ));
+
+//       var fundsFuture = DataRequestService.trApiDataRequestService.makeRequest<TrProductSearch>(Constants.getTrProductSearchRequestString(
+//         search,
+//         SearchType.fund.name,
+//       ));
+
+//       await Future.wait([
+//         stocksFuture,
+//         cryptosFuture,
+//         derivativesFuture,
+//         fundsFuture
+//       ]);
+
+//       results = {
+//         SearchType.stock: await stocksFuture,
+//         SearchType.crypto: await cryptosFuture,
+//         SearchType.derivatives: await derivativesFuture,
+//         SearchType.fund: await fundsFuture,
+//       };
+
+//       _cache[search] = results;
+//     }
+
+//     behaviorSubject.add(results);
+//     return results;
+//   }
+// }
+
+// enum SearchType {
+//   stock,
+//   crypto,
+//   fund,
+//   derivatives
+// }
+
 import 'package:rxdart/subjects.dart';
 import 'package:solidtrade/data/models/common/constants.dart';
 import 'package:solidtrade/data/models/request_response/request_response.dart';
@@ -9,15 +75,14 @@ class TrProductSearchService extends IService<TrProductSearch?> {
   TrProductSearchService() : super(BehaviorSubject.seeded(null));
   final Map<String, TrProductSearch> _cache = {};
 
-  Future<RequestResponse<TrProductSearch>> requestTrProductSearch(String search) async {
+  Future<RequestResponse<TrProductSearch>> requestTrProductSearch(SearchCategory category, String search) async {
     RequestResponse<TrProductSearch>? result;
     if (_cache.containsKey(search)) {
       result = RequestResponse.successful(_cache[search]!);
     } else {
       result = await DataRequestService.trApiDataRequestService.makeRequest<TrProductSearch>(Constants.getTrProductSearchRequestString(
         search,
-        // TODO: Currently only stocks will be shown. Fix...
-        _SearchType.stock.name,
+        category.name,
       ));
 
       if (result.isSuccessful) {
@@ -33,4 +98,9 @@ class TrProductSearchService extends IService<TrProductSearch?> {
   }
 }
 
-enum _SearchType { stock, crypto, fund, derivatives }
+enum SearchCategory {
+  stock,
+  crypto,
+  fund,
+  derivatives
+}
